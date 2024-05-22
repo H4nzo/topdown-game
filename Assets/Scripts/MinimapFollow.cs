f63cdb7c24bf2216409b25d64c202db364be346a
H4nzo <westtmu@gmail.com>
2024-05-22 13:11:20 +0100

    Playable Gameplay Feature

--- Assets/Scripts/MinimapFollow.cs
+++ Assets/Scripts/MinimapFollow.cs
@@ -12,6 +12,9 @@ public class MinimapFollow : MonoBehaviour
             // Update the position of the minimap camera to follow the player
             Vector3 newPosition = new Vector3(player.position.x, yOffset, player.position.z);
             transform.position = newPosition;
+
+            // Rotate the minimap camera to match the player's rotation
+            transform.rotation = Quaternion.Euler(90f, player.eulerAngles.y, 0f);
         }
     }
 }
